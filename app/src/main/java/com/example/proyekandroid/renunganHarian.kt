c8b0caa24764cfdad36a6539a065766d405997f3
Richson Sedjie <c14190185@john.petra.ac.id>
2021-11-26 00:22:36 +0800

    Prayer App

--- app/src/main/java/com/example/proyekandroid/renunganHarian.kt
+++ app/src/main/java/com/example/proyekandroid/renunganHarian.kt
@@ -1,13 +1,19 @@
 package com.example.proyekandroid
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
+import com.example.proyekandroid.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,11 +27,17 @@ private const val ARG_PARAM2 = "param2"
  * Use the [renunganHarian.newInstance] factory method to
  * create an instance of this fragment.
  */
-class renunganHarian : Fragment() {
+class renunganHarian : Fragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     val db = Firebase.firestore
+    lateinit var _pickDate : ImageView
+    lateinit var _tvJudulRenungan : TextView
+    lateinit var _tvIsiRenungan : TextView
+    lateinit var _tvTanggal : TextView
+
+    //lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,25 +55,115 @@ class renunganHarian : Fragment() {
         return inflater.inflate(R.layout.fragment_renungan_harian, container, false)
     }
 
+    var day = 0
+    var month = 0
+    var year = 0
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var currentDate = 0
+
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+        val reference = db.collection("Renungan")
+
+        savedDay = dayOfMonth
+        savedMonth = month
+        savedYear= year
+
+        _tvTanggal.text = "$savedDay/$savedMonth/$savedYear"
+
+//        reference.whereEqualTo("tanggal", _tvTanggal.text).addSnapshotListener { snapshot, e ->
+//            if (e != null){
+//                println ("Listen failed." + e)
+//                return@addSnapshotListener
+//            }
+//            for (document in snapshot!!){
+//                _tvJudulRenungan.text = document.data.getValue("judul").toString()
+//                _tvIsiRenungan.text = document.data.getValue("isi").toString()
+//            }
+//
+//        }
+
+    }
+
+    override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {
+
+    }
+
+    private fun getDateTimeCalendar(){
+        val cal : Calendar = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONDAY)
+        year = cal.get(Calendar.YEAR)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val _tvJudulRenungan = view.findViewById<TextView>(R.id.tvJudulRenungan)
-        val _tvIsiRenungan = view.findViewById<TextView>(R.id.tvIsiRenungan)
-        val _tvTanggal = view.findViewById<TextView>(R.id.tvTanggal)
+        _tvJudulRenungan = view.findViewById<TextView>(R.id.tvJudulRenungan)
+        _tvIsiRenungan = view.findViewById<TextView>(R.id.tvIsiRenungan)
+        _tvTanggal = view.findViewById<TextView>(R.id.tvTanggal)
         val reference = db.collection("Renungan")
+        val _gambarRenungan = view.findViewById<ImageView>(R.id.gambarRenungan)
+        _pickDate = view.findViewById<ImageView>(R.id.pickDate)
 
         val sdf = SimpleDateFormat("dd/M/yyyy")
-        val currentDate = sdf.format(Date())
+        val sdfGambar = SimpleDateFormat("dd-M-yyyy")
+        var currentDate = sdf.format(Date())
+        val currentImage = sdfGambar.format(Date())
         _tvTanggal.text = currentDate
 
+
+        _pickDate.setOnClickListener{
+            DatePickerDialog(requireActivity(), this, year, month, day).show()
+            //println(year +  month + day)
+            //println("Dari tombol" + currentDate)
+
+            println("TESSSS" + savedYear)
+            reference.whereEqualTo("tanggal", _tvTanggal.text).get()
+                .addOnCompleteListener {
+                    for (document in it.result!!){
+                        _tvJudulRenungan.text = document.data.getValue("judul").toString()
+                        _tvIsiRenungan.text = document.data.getValue("isi").toString()
+                    }
+                }
+        }
+
+        //println(currentDate)
+
         //System.out.println(" C DATE is  "+ currentDate)
-        reference.whereEqualTo("tanggal", currentDate).get()
+        reference.whereEqualTo("tanggal", _tvTanggal.text).get()
             .addOnCompleteListener {
                 for (document in it.result!!){
                     _tvJudulRenungan.text = document.data.getValue("judul").toString()
                     _tvIsiRenungan.text = document.data.getValue("isi").toString()
                 }
+            }
+
+//        reference.whereEqualTo("tanggal", _tvTanggal.text).addSnapshotListener { snapshot, e ->
+//            if (e != null){
+//                println ("Listen failed." + e)
+//                return@addSnapshotListener
+//            }
+//            for (document in snapshot!!){
+//                _tvJudulRenungan.text = document.data.getValue("judul").toString()
+//                _tvIsiRenungan.text = document.data.getValue("isi").toString()
+//            }
+//
+//        }
+
+        //Retrieve Image dari Firebase Storage
+        val imageRef = FirebaseStorage.getInstance().reference.child("Renungan/$currentImage.jpg")
+        val localFile = File.createTempFile("tempImage","jpg")
+
+        imageRef.getFile(localFile)
+            .addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+            _gambarRenungan.setImageBitmap(bitmap)
+            }
+            .addOnFailureListener{
+                Toast.makeText(activity, "Failed To Retrieve Image", Toast.LENGTH_LONG).show()
             }
 
     }
@@ -85,4 +187,7 @@ class renunganHarian : Fragment() {
                 }
             }
     }
+
+
+
 }
